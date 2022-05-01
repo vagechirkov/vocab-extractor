c4bbbedc76eefe1317a4c1091a20a9ae247d15b1
vagechirkov <vagechirkov@gmail.com>
2022-05-01 14:21:23 +0200

    update files to deploy on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vocab_extractor.wsgi
--- vocab_extractor/vocab_extractor/settings.py
+++ vocab_extractor/vocab_extractor/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d6wjly!rodbm32s#i2w_z8j)&38(f(%0+r=^epd5md@31p13)i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -114,9 +114,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
